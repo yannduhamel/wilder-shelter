04d5acbc628b10fafbad2a7b6aaea2a838586c8f
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-05-16 19:13:50 +0200

    Tous les liens vers refuge sont fonctionnels

--- src/components/InfoMassif.jsx
+++ src/components/InfoMassif.jsx
@@ -21,24 +21,42 @@ function InfoMassif({ pyrenees, jura, alpes, massifCentral }) {
       ))}
       <h3>Alpes</h3>
       {alpes.map((unRefuge) => (
-        <p key={unRefuge.id}>
-          {unRefuge.properties.nom} | Altitude : {unRefuge.properties.coord.alt}{" "}
-          m
-        </p>
+        <Link
+          key={unRefuge.id}
+          to={`/refuge/${unRefuge.id}`}
+          state={{ unRefuge }}
+        >
+          <p>
+            {unRefuge.properties.nom} | Altitude :{" "}
+            {unRefuge.properties.coord.alt} m
+          </p>
+        </Link>
       ))}
       <h3>Jura</h3>
       {jura.map((unRefuge) => (
-        <p key={unRefuge.id}>
-          {unRefuge.properties.nom} | Altitude : {unRefuge.properties.coord.alt}{" "}
-          m
-        </p>
+        <Link
+          key={unRefuge.id}
+          to={`/refuge/${unRefuge.id}`}
+          state={{ unRefuge }}
+        >
+          <p>
+            {unRefuge.properties.nom} | Altitude :{" "}
+            {unRefuge.properties.coord.alt} m
+          </p>
+        </Link>
       ))}
       <h3>Massif Central</h3>
       {massifCentral.map((unRefuge) => (
-        <p key={unRefuge.id}>
-          {unRefuge.properties.nom} | Altitude : {unRefuge.properties.coord.alt}{" "}
-          m
-        </p>
+        <Link
+          key={unRefuge.id}
+          to={`/refuge/${unRefuge.id}`}
+          state={{ unRefuge }}
+        >
+          <p>
+            {unRefuge.properties.nom} | Altitude :{" "}
+            {unRefuge.properties.coord.alt} m
+          </p>
+        </Link>
       ))}
     </section>
   );
