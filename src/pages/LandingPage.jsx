ccb052a7c0eab568ce940489372f774d2caa1346
Yann Duhamel <yann.duhamel@gmail.com>
2024-05-17 19:58:36 +0200

    fix: modified logo path for vercel deployment

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,15 +1,12 @@
 import { NavLink } from "react-router-dom";
 import "../styles/LandingPage.css";
+import logo from "../assets/shelter-logo.png";
 
 function LandingPage() {
   return (
     <div className="landing-page">
       <nav className="landing-nav">
-        <img
-          src={"src/assets/shelter-logo.png"}
-          alt="Logo de Wilder Shelter"
-          className="landing-logo"
-        />
+        <img src={logo} alt="Logo de Wilder Shelter" className="landing-logo" />
       </nav>
       <section className="landing-info">
         <h1>WILDER SHELTER</h1>
